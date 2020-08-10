13542d34cfe5e7dd8fd0ae11708a275b37f5047d
ayao1998 <57114330+ayao1998@users.noreply.github.com>
2020-08-09 18:27:27 -0700

    Add files via upload
    -changed to system L&F.
    -changed "View Cars" and main menu to GridLayout.
    -changed JButton font sizes on main menu,
    -changed spec JPanel font sizes. 
    -rearranged specs in order of typical importance.
    -fixed main menu JButtons not appearing until cursor is placed over them (windowListener).

--- database.java
+++ database.java
@@ -13,7 +13,10 @@ import java.awt.event.WindowEvent;
 public class database {
 	public static void main(String args[])
 	{
-
+//to do : 
+	// arrange car buttons in view all cars (add sort by year and make drop down)
+		//fix formatting and make prettier. (put space between buttons and jframe)
+		//add more cars
 		
 		//**SETUP
 		ArrayList<Car> myCars = new ArrayList<Car>(); 
@@ -48,13 +51,31 @@ public class database {
 		
 		//**JFRAME STUFF
 		
-		
-		
+	    try {
+            // Set System L&F
+        UIManager.setLookAndFeel(
+            UIManager.getSystemLookAndFeelClassName());
+	    } 
+	    catch (UnsupportedLookAndFeelException e) {
+	       // handle exception
+	    }
+	    catch (ClassNotFoundException e) {
+	       // handle exception
+	    }
+	    catch (InstantiationException e) {
+	       // handle exception
+	    }
+	    catch (IllegalAccessException e) {
+	       // handle exception
+	    }
+
+    
+    
 		JFrame frame = new JFrame("Car Database");  //arg is what the program window says
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //click x to end program
+		//frame.setLayout(new BoxLayout(frame, BoxLayout.PAGE_AXIS));
 		frame.setSize(415, 290); //size of window
 		frame.setLocationRelativeTo(null); 
-		frame.setVisible(true); //set to true
 		frame.addWindowListener(new WindowAdapter(){
 
 		    public void windowClosing(WindowEvent e){
@@ -64,14 +85,14 @@ public class database {
 		});
 		
 		JPanel mainPanel = new JPanel();
-		frame.add(mainPanel);
-		//mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+		mainPanel.setLayout(new GridLayout(3,1,10,10));
 		mainPanel.setSize(415, 290); //size of window
 		mainPanel.setVisible(true); //set to true
-		
+		frame.add(mainPanel);
 		
 		JButton button = new JButton("View cars"); //make a button named "arg"
 		button.setBounds(25, 25, 350, 50); // x,y coords, x size y size
+		button.setFont(new Font("TimesRoman", Font.PLAIN, 40));
 		mainPanel.add(button); //add button to frame
 		button.addActionListener(new ActionListener() 
 		{
@@ -87,6 +108,7 @@ public class database {
 		
 		JButton button2 = new JButton("Add a new car");
 		button2.setBounds(25, 100, 350, 50);
+		button2.setFont(new Font("TimesRoman", Font.PLAIN, 40));
 		mainPanel.add(button2);
 		
 		
@@ -100,6 +122,7 @@ public class database {
 		
 		JButton button3 = new JButton("Exit");
 		button3.setBounds(25, 175, 350, 50);
+		button3.setFont(new Font("TimesRoman", Font.PLAIN, 40));
 		mainPanel.add(button3);
 		
 		
@@ -111,6 +134,7 @@ public class database {
 			System.exit(0);
 			}
 		});	
+		frame.setVisible(true); //set to true
 		//CLOSING THE PROGRAM***
 		input.close();
 
@@ -123,21 +147,24 @@ public class database {
 			final ArrayList<Car> final_cars = myCars;
 			
 		
-			JFrame frame = new JFrame("All cars"); 
-			frame.setLocation(700, 0); //puts frame in middle
+			JFrame frame = new JFrame("All cars");  
+			frame.setLocation(700, 400); //puts frame in middle
 			frame.setSize(500, 200);
 			frame.setVisible(true);
 			
 			JPanel panel = new JPanel();
-			panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+			panel.setLayout(new GridLayout(3,3,10,10));
 			
 			Iterator<Car> buttIt = final_cars.iterator();
 			Car tempCar;
+			
+			int year = Calendar.getInstance().get(Calendar.YEAR);
 			
 			while(buttIt.hasNext())
 			{
 				tempCar = buttIt.next();
 				JButton tempButt = new JButton(tempCar.year + " " + tempCar.make + " " + tempCar.model);
+				
 				panel.add(tempButt);
 				final Car[] tester_final = new Car[1];
 				tester_final[0] = tempCar; //  https://stackoverflow.com/questions/4732544/why-are-only-final-variables-accessible-in-anonymous-class
@@ -147,43 +174,44 @@ public class database {
 					{
 						
 						//view car on new jFrame
+
 						JFrame carFrame = new JFrame(tester_final[0].getYear() + " " + tester_final[0].getMake() + " " + tester_final[0].getModel());
 						carFrame.setSize(400, 300);
 						carFrame.setVisible(true);
+						carFrame.setLocation(200, 100);
 						
 						JPanel spec_panel = new JPanel();
 						spec_panel.setLayout(new BoxLayout(spec_panel, BoxLayout.PAGE_AXIS));
 						
-						
 						JLabel year_make_model = new JLabel(tester_final[0].getYear() + " " + tester_final[0].getMake() + " " + tester_final[0].getModel());
-						JLabel mpgLabel = new JLabel(tester_final[0].getMPG());
-						JLabel hpLabel = new JLabel(tester_final[0].getHP());
-						JLabel weightLabel = new JLabel(tester_final[0].getWeight());
-						JLabel transLabel = new JLabel(tester_final[0].getTrans());
-						JLabel engineLabel = new JLabel(tester_final[0].getEngine());
-						JLabel bodyLabel = new JLabel(tester_final[0].getBodyStyle());
-						JLabel sizeLabel = new JLabel(tester_final[0].getSize());
+						JLabel age = new JLabel("Age: " + (year-tester_final[0].getYear()) + " year(s)");
+						JLabel mpgLabel = new JLabel("MPG: " + tester_final[0].getMPG());
+						JLabel hpLabel = new JLabel("Horsepower: " + tester_final[0].getHP());
+						JLabel weightLabel = new JLabel("Curb weight: " + tester_final[0].getWeight());
+						JLabel transLabel = new JLabel("Transmission: " + tester_final[0].getTrans());
+						JLabel engineLabel = new JLabel("Engine: " + tester_final[0].getEngine());
+						JLabel bodyLabel = new JLabel("Body style: " + tester_final[0].getBodyStyle());
+						JLabel sizeLabel = new JLabel("Size/class: " + tester_final[0].getSize());
 						
-						
-//trying to figure out how to pass in the car details do print on a jframe at the click of button.
-					//attempting to make a new class that implements actionlistener.
-						
-						/*
-						car_deets.setAlignmentX(0);
-						car_deets.setAlignmentY(0);
-						car2.setAlignmentX(100);
-						car2.setAlignmentY(100);
-						*/
+	
 						year_make_model.setFont(new Font("", Font.PLAIN, 30));
-						
+						age.setFont(new Font("", Font.PLAIN, 20));
+						bodyLabel.setFont(new Font("", Font.PLAIN, 20));
+						sizeLabel.setFont(new Font("", Font.PLAIN, 20));
+						mpgLabel.setFont(new Font("", Font.PLAIN, 20));
+						hpLabel.setFont(new Font("", Font.PLAIN, 20));
+						engineLabel.setFont(new Font("", Font.PLAIN, 20));
+						transLabel.setFont(new Font("", Font.PLAIN, 20));
+						weightLabel.setFont(new Font("", Font.PLAIN, 20));
 						spec_panel.add(year_make_model);
-						spec_panel.add(mpgLabel);
-						spec_panel.add(hpLabel);
-						spec_panel.add(weightLabel);
-						spec_panel.add(transLabel);
-						spec_panel.add(engineLabel);
+						spec_panel.add(age);
 						spec_panel.add(bodyLabel);
 						spec_panel.add(sizeLabel);
+						spec_panel.add(mpgLabel);
+						spec_panel.add(hpLabel);
+						spec_panel.add(engineLabel);
+						spec_panel.add(transLabel);
+						spec_panel.add(weightLabel);
 						
 						carFrame.add(spec_panel);
 						
@@ -192,7 +220,19 @@ public class database {
 				});
 			}
 			
+			/*
+			String[] sortOptions = {"No sort", "Sort by Year", "Sort by Make"};
+			JComboBox sorting = new JComboBox(sortOptions);
+			sorting.addActionListener(new ActionListener()
+			{ public void actionPerformed(ActionEvent arg0)
+				{
+						
+				}
 			
+			});
+			panel.add(sorting);
+			
+			*/
 			frame.add(panel, BorderLayout.NORTH);
 				   
 			
@@ -260,22 +300,67 @@ public class database {
 		public static void inputCar(ArrayList<Car> carList)
 		{
 			JFrame frame = new JFrame("Enter new car");
-			JTextField text = new JTextField(25);
+			JPanel panel = new JPanel();
+			panel.setLayout(new SpringLayout());
+			panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+			panel.setSize(500, 380);
+			
+			JLabel ymm = new JLabel("Year Make Model (e.g. 2018 Honda Civic): ");
+			JTextField text = new JTextField(10);
+			panel.add(ymm);
+			panel.add(text);
+			
+			JLabel bodyStyle = new JLabel("Body Style (e.g. Sedan/Coupe): ");
+			JTextField text7 = new JTextField(10);
+			panel.add(bodyStyle);
+			panel.add(text7);
+			
+			JLabel size = new JLabel("Size (e.g. Fullsize): ");
+			JTextField text8 = new JTextField(10);
+			panel.add(size);
+			panel.add(text8);
+			
+			JLabel mpg = new JLabel("MPG (e.g. 30city/40hwy): ");
+			JTextField text2 = new JTextField(10);
+			panel.add(mpg);
+			panel.add(text2);
+			
+			JLabel hp = new JLabel("HP (e.g. 400hp-555hp): ");
+			JTextField text3 = new JTextField(10);
+			panel.add(hp);
+			panel.add(text3);
+			
+			JLabel engine = new JLabel("Engine (e.g. I4(1.8L-T)/V8(5.2L)): ");
+			JTextField text6 = new JTextField(10);
+			panel.add(engine);
+			panel.add(text6);
+			
+			JLabel trans = new JLabel("Transmission (e.g. Automatic(3spd)/Manual(6spd)): ");
+			JTextField text5 = new JTextField(10);
+			panel.add(trans);
+			panel.add(text5);
+			
+			JLabel weight = new JLabel("Weight (e.g. 2,500lbs-2,700lbs): ");
+			JTextField text4 = new JTextField(10);
+			panel.add(weight);
+			panel.add(text4);
+
 			JButton enterInput = new JButton("Enter");
-			frame.getContentPane().add(text, BorderLayout.WEST);
+		  
 			
 			enterInput.addActionListener(new ActionListener() {
 				@Override public void actionPerformed(ActionEvent arg0) {
 					String car = text.getText();
 					String carSplit[] = car.split(" ");
-					Car tempCar = new Car(Integer.parseInt(carSplit[0]), carSplit[1], carSplit[2], carSplit[3], carSplit[4], carSplit[5], carSplit[6], carSplit[7], carSplit[8], carSplit[9]);
+					Car tempCar = new Car(Integer.parseInt(carSplit[0]), carSplit[1], carSplit[2], text2.getText(), text3.getText(), text4.getText(), text5.getText(), text6.getText(), text7.getText(), text8.getText());
 					carList.add(tempCar);
 					frame.dispose();
 					
 				}
 			});
-			frame.add(enterInput);
-			frame.setSize(500, 100); //size of window
+			panel.add(enterInput);
+			frame.add(panel);
+			frame.setSize(500, 380); //size of window
 		      frame.setLocation(700, 250); //puts frame in middle
 	
 		      frame.setVisible(true);
